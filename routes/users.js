89e6c9538e5c66b9443bd83040914adf61ca53f7
HuMiao <humiao2006@gmail.com>
2021-08-13 16:50:47 +0800

    xxoo

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,9 @@ function getInterval(type) {
         case 'cheshihao':
             interval = 3000
             break;
+        case 'cheshihao_video':
+            interval = 3000
+            break;
         default:
             interval = 3000
             break;
@@ -22,28 +25,41 @@ function getInterval(type) {
 
 async function run(urls) {
     let arr = [
-        'http://aikahao.xcar.com.cn/item/910313.html',
+        'http://www.toutiao.com/a1707672883274759/',
+        'http://www.toutiao.com/a6994727135298473229/',
+        // 'http://aikahao.xcar.com.cn/item/910313.html',
         // 'http://aikahao.xcar.com.cn/item/910054.html',
         // 'http://aikahao.xcar.com.cn/item/909846.html',
         // 'http://aikahao.xcar.com.cn/item/910147.html',
         // 'http://aikahao.xcar.com.cn/item/910945.html',
         // 'http://cheshihao.cheshi.com/news/557752.html',
         // 'http://cheshihao.cheshi.com/news/558130.html',
-        'http://cheshihao.cheshi.com/news/557548.html',
+        // 'http://cheshihao.cheshi.com/news/557548.html',
+        // 'http://cheshihao.cheshi.com/video/558485.html',
         // 'http://cheshihao.cheshi.com/news/557887.html',
         // 'http://cheshihao.cheshi.com/news/558238.html',
         // 'http://cheshihao.cheshi.com/news/557759.html'
     ]
     let arrUrls = []
     for (let i = 0; i < arr.length; i++) {
-        let type
+        let type = '';
+        let platform = '';
         if (arr[i].indexOf('aikahao.xcar.com.cn/item') > -1) {
             type = 'aikahao'
+            platform = '爱咖号'
         } else if (arr[i].indexOf('cheshihao.cheshi.com/news') > -1) {
             type = 'cheshihao'
+            platform = '车市号'
+        } else if (arr[i].indexOf('cheshihao.cheshi.com/video') > -1) {
+            type = 'cheshihao_video'
+            platform = '车市号视频'
+        } else if (arr[i].indexOf('toutiao.com') > -1) {
+            type = 'toutiao'
+            platform = '今日头条'
         }
         arrUrls.push({
             type,
+            platform,
             url: arr[i],
             id: i
         })
@@ -78,13 +94,14 @@ async function run(urls) {
     let promiseArr = []
     for (let i = 0; i < sites.length; i++) {
         // console.log("sites[i]",sites[i])
-        promiseArr.push(openNewPage(sites[i],  i))
+        promiseArr.push(openNewPage(sites[i], i))
         // console.log('promiseArr.push' + i)
     }
     Promise.all(promiseArr).then(() => {
         console.log('all complete')
     })
 }
+
 // let json = []
 // for (let i = 0; i < emojis.length; i++) {
 //     const name = emojis[i].slice(emojis[i].lastIndexOf('/') + 1)
@@ -98,47 +115,112 @@ async function run(urls) {
 // }
 // // 写入json文件
 // fs.writeFile('./google-emoji.json', JSON.stringify(json), function () {})
-
+//const browser = await puppeteer.launch({ignoreDefaultArgs: ["--enable-automation"], headless: true}); //去除自动化测试的提醒
+//   const page = await browser.newPage();
+//   await page.evaluateOnNewDocument(() => { //在每个新页面打开前执行以下脚本
+//     const newProto = navigator.__proto__;
+//     delete newProto.webdriver;  //删除navigator.webdriver字段
+//     navigator.__proto__ = newProto;
+//     window.chrome = {};  //添加window.chrome字段，为增加真实性还需向内部填充一些值
+//     window.chrome.app = {"InstallState":"hehe", "RunningState":"haha", "getDetails":"xixi", "getIsInstalled":"ohno"};
+//     window.chrome.csi = function(){};
+//     window.chrome.loadTimes = function(){};
+//     window.chrome.runtime = function(){};
+//     Object.defineProperty(navigator, 'userAgent', {  //userAgent在无头模式下有headless字样，所以需覆写
+//         get: () => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36",
+//     });
+//     Object.defineProperty(navigator, 'plugins', {  //伪装真实的插件信息
+//         get: () => [{"description": "Portable Document Format",
+//                     "filename": "internal-pdf-viewer",
+//                     "length": 1,
+//                     "name": "Chrome PDF Plugin"}]
+//     });
+//     Object.defineProperty(navigator, 'languages', { //添加语言
+//         get: () => ["zh-CN", "zh", "en"],
+//     });
+//     const originalQuery = window.navigator.permissions.query; //notification伪装
+//     window.navigator.permissions.query = (parameters) => (
+//     parameters.name === 'notifications' ?
+//       Promise.resolve({ state: Notification.permission }) :
+//       originalQuery(parameters)
+//   );
+//
+//   await page.goto("https://www.aabbccc.com");
+//   //...
+//   await browser.close();
 
 function getData(browser, record, i) {
     return new Promise(async (resolve, reject) => {
         console.log("getData:", record.url, i, record.id, record.type)
         // console.log('__dirname : ' + __dirname)
+
         const page = await browser.newPage();
-        await page.goto(record.url,{ waitUntil: 'networkidle0', timeout: 30 * 1000 }).catch(() =>{
+        await page.goto(record.url, {waitUntil: 'networkidle0', timeout: 30 * 1000}).catch(() => {
             console.log('超时')
         })
         // getSelector(record.type)
-        // await page.waitFor('.browse_number')
+        await page.waitFor(5000)
         let title = await page.title();
-        await page.addScriptTag({path:'./jquery.js'})
-        let result = await page.evaluate(() =>{
-            Object.defineProperties(navigator,{
-                webdriver:{
+        await page.addScriptTag({path: './jquery.js'})
+        let result = await page.evaluate((type) => {
+
+            function findDataByType(jquery, type) {
+                let data = {}
+                switch (type) {
+                    case 'aikahao':
+                        data.read = jquery('.browse_number').text().replace(/[^\d.]/g, "")
+                        data.author = jquery('.detail_txt_lf').find('a').text().trim()
+                        break;
+                    case 'cheshihao':
+                        data.read = jquery('.icon_browse').parent().text().replace(/[^\d.]/g, "")
+                        data.author = jquery('.name').text().trim()
+                        break;
+                    case 'cheshihao_video':
+                        data.read = jquery('.icon_browse').parent().text().replace(/[^\d.]/g, "")
+                        data.author = jquery('.name').text().trim()
+                        break;
+                    case 'toutiao':
+                        // console.log('length:', )jquery('.playerContainer').length
+                        // if(jquery('.playerContainer').length > 0){
+                        //     data.platform = '今日头条视频'
+                        //     data.isVideo = true
+                        // }else{
+                        //     data.platform = '今日头条'
+                        //     data.isVideo = false
+                        // }
+                        data.author = jquery('.user__name').text().trim()
+                        if(!data.author){
+                            data.author = jquery('.name').text().trim()
+                        }
+                        // data.read = jquery('.icon_browse').parent().text().replace(/[^\d.]/g, "")
+                        break;
+                    default:
+                        break;
+                }
+                return data
+            }
+
+            Object.defineProperties(navigator, {
+                webdriver: {
                     get: () => false
                 }
             })
-            var miaoQuery = jQuery.noConflict();
-            let read =  miaoQuery('.browse_number').text().replace(/[^\d.]/g, "")
-            let author =  miaoQuery('.detail_txt_lf').find('a').text().trim()
-            console.log('read:', read)
-            console.log('author:', author)
-            return {
-                read,
-                author
-            }
-        }, '');
-        console.log('result', result)
+            let miaoQuery = jQuery.noConflict();
+            return findDataByType(miaoQuery, type)
+        }, record.type);
 
-        resolve({
-            read:'2',
-            author:'1',
-            platform: '爱咖号',
+        // console.log('result', )
+        resolve(Object.assign({
+            read: '',
+            author: '',
+            platform: record.platform,
             title,
-            brand: '别克',
-            model: '微蓝6',
-            id: record.id
-        })
+            brand: '',
+            model: '',
+            isVideo: record.type.indexOf('_video') > -1,
+            id: record.id,
+            remark: ''
+        }, result))
     })
 }
 
@@ -153,7 +235,8 @@ async function openNewPage(site, index) {
             defaultViewport: {
                 width: 1920,
                 height: 1080,
-            }
+            },
+            headless: true
         });
         let data = []
         for (let i = 0; i < site.urls.length; i++) {
@@ -162,7 +245,7 @@ async function openNewPage(site, index) {
             data.push(record)
             await sleeep(site.interval)
         }
-        console.log('第' + index + '个网站:'+site.type +'完成')
+        console.log('第' + index + '个网站:' + site.type + '完成')
         await browser.close();
         resolve(data)
     })
